761b00cf37f66266c00c76731b97d2928af06525
Gerardo Mayel Fernández Alamilla <geramfernandez@gmail.com>
2023-10-27 19:55:09 -0600

    tarea calculadora equipo 3 se carga extemporanea el 27 de octubre

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
-from operaciones.suma import suma
-from operaciones.resta import resta
-from operaciones.multiplicacion import multiplicacion
-from operaciones.division import division
-from operaciones.fracciones_a_numero import fraccion_a_numero
+from operaciones.suma import sumar
+from operaciones.resta import restar
+from operaciones.multiplicacion import multiplicar
+from operaciones.division import dividir
+
+
+def is_number(value):
+    """Verifica si el valor es un número."""
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
 
 if __name__ == "__main__":
-    # Aquí puedes poner pruebas simples para verificar la funcionalidad
-    print(suma(5, 5))
+    # Entrada del usuario
+    while True:
+        num1 = input("Ingresa el primer número (o 'salir' para terminar): ")
+        if num1.lower() == 'salir':
+            break
+
+        if not is_number(num1):
+            print("Error: El primer número ingresado no es válido.")
+            continue
+
+        num2 = input("Ingresa el segundo número: ")
+
+        if not is_number(num2):
+            print("Error: El segundo número ingresado no es válido.")
+            continue
+
+        operacion = input("Elige una operación (+, -, *, /): ")
+
+        try:
+            if operacion == '+':
+                resultado = sumar(num1, num2)
+            elif operacion == '-':
+                resultado = restar(num1, num2)
+            elif operacion == '*':
+                resultado = multiplicar(num1, num2)
+            elif operacion == '/':
+                resultado = dividir(num1, num2)
+            else:
+                print("Operación no válida.")
+                continue
+
+            print(f"Resultado: {resultado}\n")
+        except ValueError as e:
+            print(f"{e}\n")
